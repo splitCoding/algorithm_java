f4437037ed52a984c5bf896c21b0ca454cd117dd
splitube <voicesplit@daum.net>
2022-01-26 21:30:49 +0900

    testcase 추가

--- src/programmers/Problem_42577.java
+++ src/programmers/Problem_42577.java
@@ -2,7 +2,9 @@ package programmers;
 
 public class Problem_42577 {
     public static void main(String[] args) {
-
+        System.out.println(new Solution().solution(new String[]{"119", "97674223", "1195524421"}));
+        System.out.println(new Solution().solution(new String[]{"123", "456", "789"}));
+        System.out.println(new Solution().solution(new String[]{"12", "123", "1235", "567", "88"}));
     }
 
     static class Solution {
