c1c8b3ef1cc284920772a4bcdfed9ad81e9179d1
splitube <voicesplit@daum.net>
2022-01-26 21:33:03 +0900

    String [] phone_book 길이 오름차순으로 정렬
    Arrays.sort()와 Comparator 사용

--- src/programmers/Problem_42577.java
+++ src/programmers/Problem_42577.java
@@ -1,5 +1,7 @@
 package programmers;
 
+import java.util.*;
+
 public class Problem_42577 {
     public static void main(String[] args) {
         System.out.println(new Solution().solution(new String[]{"119", "97674223", "1195524421"}));
@@ -9,6 +11,14 @@ public class Problem_42577 {
 
     static class Solution {
         boolean solution(String[] phone_book) {
+            Arrays.sort(phone_book, new Comparator<String>() {
+                @Override
+                public int compare(String o1, String o2) {
+                    if(o1.length() == o2.length())
+                        return o1.compareTo(o2);
+                    return o1.length() - o2.length();
+                }
+            });
             return true;
         }
     }
