5a2f412923001448c78e5a09484381d80c4de78c
splitube <voicesplit@daum.net>
2022-01-26 21:29:59 +0900

    Solution 내부 클래스 생성
    문제의 답을 리턴하는 boolean solution(String[] phone_book)

--- src/programmers/Problem_42577.java
+++ src/programmers/Problem_42577.java
@@ -1,4 +1,13 @@
 package programmers;
 
 public class Problem_42577 {
+    public static void main(String[] args) {
+
+    }
+
+    static class Solution {
+        boolean solution(String[] phone_book) {
+            return true;
+        }
+    }
 }
