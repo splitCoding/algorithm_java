2341a49fbadb97ce7743d61d14c6db5b73766c32
splitube <voicesplit@daum.net>
2022-01-27 13:47:14 +0900

    재귀로 숫자를 조합하기 위한 rec_func 클래스 메서드 추가

--- src/beakjoon/Problem_15649.java
+++ src/beakjoon/Problem_15649.java
@@ -13,4 +13,21 @@ public class Problem_15649 {
         selected = new int[M];
         is_used = new boolean[N + 1];
     }
+
+    static void rec_func(int k) {
+        if (k == M) {
+            for (int num : selected) sb.append(num).append(" ");
+            sb.append('\n');
+        } else {
+            for (int cand = 1; cand <= N; cand++) {
+                if (!is_used[cand]) {
+                    selected[k] = cand;
+                    is_used[cand] = true;
+                    rec_func(k + 1);
+                    selected[k] = 0;
+                    is_used[cand] = false;
+                }
+            }
+        }
+    }
 }
