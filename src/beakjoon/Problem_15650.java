720f701c18261ed1e6c0888f4beb7f963b563c81
splitube <voicesplit@daum.net>
2022-01-27 15:03:51 +0900

    rec_func() 오버라이딩
    오름차순이 아니면 continue

--- src/beakjoon/Problem_15650.java
+++ src/beakjoon/Problem_15650.java
@@ -1,4 +1,21 @@
 package beakjoon;
 
 public class Problem_15650 extends Problem_15649 {
+    static void rec_func(int k) {
+        if (k == M) {
+            for (int num : selected) sb.append(num).append(" ");
+            sb.append('\n');
+        } else {
+            for (int cand = 1; cand <= N; cand++) {
+                if (k > 0 && selected[k - 1] > cand) continue;
+                if (!is_used[cand]) {
+                    selected[k] = cand;
+                    is_used[cand] = true;
+                    rec_func(k + 1);
+                    selected[k] = 0;
+                    is_used[cand] = false;
+                }
+            }
+        }
+    }
 }
