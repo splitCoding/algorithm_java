62f74212b526bf36fc687dc75ecf7ddf7f1620ba
splitube <voicesplit@daum.net>
2022-01-27 13:46:06 +0900

    입력을 받는 input() 메서드

--- src/beakjoon/Problem_15649.java
+++ src/beakjoon/Problem_15649.java
@@ -5,4 +5,12 @@ public class Problem_15649 {
     static int N, M;
     static int[] selected;
     static boolean[] is_used;
+
+    static void input() {
+        FastReader scan = new FastReader(); //Problem_15651 브랜치에 코드 소스 존재
+        N = scan.nextInt();
+        M = scan.nextInt();
+        selected = new int[M];
+        is_used = new boolean[N + 1];
+    }
 }
