f64b7352464db5ee2de72af9bf1248b096fcab72
splitube <voicesplit@daum.net>
2022-01-27 14:33:36 +0900

    rec_func() 메서드 오버라이딩

--- src/beakjoon/Problem_15652.java
+++ src/beakjoon/Problem_15652.java
@@ -1,4 +1,17 @@
 package beakjoon;
 
 public class Problem_15652 extends Problem_15651 {
+    static void rec_func(int k) {
+        if (k == M) {
+            for (int num : selected) sb.append(num).append(" ");
+            sb.append('\n');
+        } else {
+            for (int i = 1; i <= N; i++) {
+                if (k > 0 && selected[k - 1] > i) continue;
+                selected[k] = i;
+                rec_func(k + 1);
+                selected[k] = 0;
+            }
+        }
+    }
 }
