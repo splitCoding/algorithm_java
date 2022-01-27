0375762efa6adafcf2b80fbcfda9db44aa1fee3c
splitube <voicesplit@daum.net>
2022-01-27 13:21:30 +0900

    시간단축을 위한 BufferedReader를 이용한 입력을 위한 FastReader class

--- src/beakjoon/Problem_15651.java
+++ src/beakjoon/Problem_15651.java
@@ -1,4 +1,49 @@
 package beakjoon;
 
+import java.util.*;
+import java.io.*;
+
 public class Problem_15651 {
+}
+
+class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    FastReader() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    String next() {
+        while (st == null || !st.hasMoreTokens()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+        String ret = "";
+        try {
+            ret += br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
 }
