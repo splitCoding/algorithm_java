282ea32dbf8e222d74d7eee2c04296fc2c9630e3
splitube <voicesplit@daum.net>
2022-01-27 11:20:39 +0900

    체크할 HashMap check 생성

--- src/programmers/Problem_42578.java
+++ src/programmers/Problem_42578.java
@@ -1,14 +1,17 @@
 package programmers;
 
+import java.util.*;
+
 public class Problem_42578 {
     public static void main(String[] args) {
-        System.out.println(new Solution().solution(new String[][]{{"yellowhat", "headgear"},{"bluesunglasses", "eyewear"},{"green_turban", "headgear"}}));
-        System.out.println(new Solution().solution(new String[][]{{"crowmask", "face"},{"bluesunglasses", "face"},{"smoky_makeup", "face"}}));
+        System.out.println(new Solution().solution(new String[][]{{"yellowhat", "headgear"}, {"bluesunglasses", "eyewear"}, {"green_turban", "headgear"}}));
+        System.out.println(new Solution().solution(new String[][]{{"crowmask", "face"}, {"bluesunglasses", "face"}, {"smoky_makeup", "face"}}));
     }
 
     static class Solution {
         public int solution(String[][] clothes) {
             int ret = 1;
+            HashMap<String, Integer> check = new HashMap<String, Integer>(clothes.length);
             return ret;
         }
     }
