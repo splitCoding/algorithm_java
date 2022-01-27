6ccb01c8144a9556e524982aa49b00b91aff6695
splitube <voicesplit@daum.net>
2022-01-27 13:43:53 +0900

    클래스 변수 추가
    N,M, 조합한 숫자의 배열 int [] selected, 사용한 숫자인지 확인하는 boolean [] is_used

--- src/beakjoon/Problem_15649.java
+++ src/beakjoon/Problem_15649.java
@@ -1,5 +1,8 @@
 package beakjoon;
 
 public class Problem_15649 {
-
+    static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static int[] selected;
+    static boolean[] is_used;
 }
