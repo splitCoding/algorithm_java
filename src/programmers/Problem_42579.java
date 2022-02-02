bd631bd02b28fe3f5f4ff8ff72c54c44d678b284
splitube <voicesplit@daum.net>
2022-02-02 15:43:54 +0900

    Genre class 생성
    장르가 총 플레이된 횟수, 제일 많이 재생된 곡의 재생 횟수, 다음으로 많이 재생된곡의 재생 횟수 , 제일 많이 재생된 두 곡의 인덱스

--- src/programmers/Problem_42579.java
+++ src/programmers/Problem_42579.java
@@ -1,4 +1,13 @@
 package programmers;
 
+import java.util.ArrayList;
+
 public class Problem_42579 {
+    class Genre {
+        int total_played = 0;
+        int first = 0;
+        int second = 0;
+        ArrayList<Integer> popular = new ArrayList<Integer>(2);
+    }
 }
+
