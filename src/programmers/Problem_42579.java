542358a35c9bde729ce6b44a6ae8d36ba61c2063
splitube <voicesplit@daum.net>
2022-02-03 17:28:33 +0900

    Revert "Genre class 생성"
    This reverts commit bd631bd02b28fe3f5f4ff8ff72c54c44d678b284.

--- src/programmers/Problem_42579.java
+++ src/programmers/Problem_42579.java
@@ -1,13 +1,4 @@
 package programmers;
 
-import java.util.ArrayList;
-
 public class Problem_42579 {
-    class Genre {
-        int total_played = 0;
-        int first = 0;
-        int second = 0;
-        ArrayList<Integer> popular = new ArrayList<Integer>(2);
-    }
 }
-
